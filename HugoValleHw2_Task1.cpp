754362d07d6ea6c7adf73ce0b421bdf4545c53fe
hvalle <hugovalle1@weber.edu>
2017-09-19 13:00:55 -0600

    First commit

---
+++ HugoValleHw2_Task1.cpp
@@ -0,0 +1,16 @@
+// File: HugoValleHw2_Task1
+// Created by Hugo Valle on 9/19/2017 for CS1410.
+// Copyright (c) 2017 WSU
+#include <iostream>
+using namespace std;
+// Constants
+
+// Prototypes
+
+// Main Program Program
+int main(void)
+{
+
+    return 0;
+}
+// Function Definitions
